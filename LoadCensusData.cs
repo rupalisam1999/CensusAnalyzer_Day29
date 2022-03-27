f08d87245b8d2d09d67bb560d9f03e1a18984433
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-27 22:14:55 +0530

    [Rupali]ADDED: Load Indian state census data into a Csv file

---
+++ LoadCensusData.cs
@@ -0,0 +1,43 @@
+﻿using CensusAnalyzer_Day29;
+
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CensusAnalyzer_Day29.POCO;
+using System.Globalization;
+using CensusAnalyzer_Day29.CustomException;
+
+namespace CensusAnalyzer_Day29
+{
+    public class LoadCensusData
+    {
+        List<IndianStateCensusData> indianStateCensusDatas;
+        public int LoadCensusDataIntoList(string filepath)
+        {
+            try
+            {
+                using (StreamReader reader = File.OpenText(filepath))
+
+                using (CsvReader csvreader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    indianStateCensusDatas = csvreader.GetRecords<IndianStateCensusData>().ToList();
+                }
+                return indianStateCensusDatas.Count;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new CecsusAnalyzerException("WrongHeader","WrongFileType");
+            }
+           
+            return indianStateCensusDatas.Count;
+        }
+        
+    }
+}
---
+++ POCO/IndianStateCensusData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CensusAnalyzer_Day29.POCO
+{
+     public class IndianStateCensusData
+    {
+        public string State { get; set; }
+
+        public long Population { get; set; }
+
+        public long AreaInSqKm { get; set; }
+
+        public long DensityPerSqKm { get; set; }
+
+        
+    }
+}
